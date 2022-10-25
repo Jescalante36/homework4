95278912a3e67116bd9920ce74bf35a6ebd9ea8a
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-10-24 19:51:04 -0500

    Update header.php

--- header.php
+++ header.php
@@ -32,20 +32,20 @@ echo "Connected successfully";
 
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">University of Oklahoma</a>
+    <a class="navbar-brand" href="index.php">Sport</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="basketball.php">Classes</a>
+          <a class="nav-link active" aria-current="page" href="basketball.php">Soccer</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="football.php">Classes</a>
+          <a class="nav-link" href="football.php">Basketball</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="baseball.php">Section</a>
+          <a class="nav-link" href="baseball.php">Football</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="together.php">Together</a>

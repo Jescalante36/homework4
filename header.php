578af8cb7b210edcb9f56f020519472ba971e131
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-10-25 01:31:48 -0500

    Update header.php

--- header.php
+++ header.php
@@ -30,7 +30,7 @@ echo "Connected successfully";
 ?>
 
 
-<nav class="navbar navbar-expand-lg bg-dark">
+<nav class="navbar navbar-expand-lg bg-primary">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">Sport</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">

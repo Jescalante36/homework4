c2ad4f2280d135a999dc0cea489728ae60659682
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-10-24 23:17:25 -0500

    Update baseball.php

--- baseball.php
+++ baseball.php
@@ -39,7 +39,7 @@ if ($result->num_rows > 0) {
   ?> 
    </tbody>
 </table>
-</html>
+
 
 
 
@@ -70,3 +70,4 @@ if ($result->num_rows > 0) {
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</html>

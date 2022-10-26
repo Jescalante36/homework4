6933d70de8333f6b4bd54451f6b72a5fa8a97eae
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-10-26 12:39:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<H2 style="color:Tomato;"> Please select any of the provided sports to add new athletes</h2>
+<H2 style="background-color:DodgerBlue;"> Please select any of the provided sports to add new athletes</h2>
 <?php require_once("header.php"); ?>
 
 
